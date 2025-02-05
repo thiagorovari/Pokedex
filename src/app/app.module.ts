a34a1bc52ae88012d66f5dd02ae7673330d03c18
Thiago <thiagorovari@gmail.com>
2025-02-05 20:22:20 -0300

    Adicionando direcionamento do click para aba details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent
