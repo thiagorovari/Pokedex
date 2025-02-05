9172fd696433fc695e61a496033c6f9d6c357848
Thiago <thiagorovari@gmail.com>
2025-02-05 10:12:45 -0300

    Adicionando poke header

---
+++ src/app/shared/poke-header/poke-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'poke-header',
+  standalone: false,
+  
+  templateUrl: './poke-header.component.html',
+  styleUrl: './poke-header.component.scss'
+})
+export class PokeHeaderComponent {
+
+}
