39825fbd47c62308bc41448fea2ce14d8efa9a7d
Thiago <thiagorovari@gmail.com>
2025-02-05 21:07:15 -0300

    Funcionalidade da barra de pesquisa

--- src/app/shared/poke-search/poke-search.component.ts
+++ src/app/shared/poke-search/poke-search.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'poke-search',
-  standalone: false,
-  
   templateUrl: './poke-search.component.html',
-  styleUrl: './poke-search.component.scss'
+  styleUrls: ['./poke-search.component.scss'],
+  standalone: false
 })
-export class PokeSearchComponent {
+export class PokeSearchComponent implements OnInit {
+  @Output() public emmitSearch: EventEmitter<string> = new EventEmitter();
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public search(value: string){
+    this.emmitSearch.emit(value);
+  }
 }
